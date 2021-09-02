1f91b19a19649df106969257fa828a433b1f9c7e
Bruno Vilela <brunovilelasilva@hotmail.com>
2021-09-02 09:52:03 -0300

    Update lcvp_solve_dups documentation

--- R/lcvp_solve_dups.R
+++ R/lcvp_solve_dups.R
@@ -1,14 +1,17 @@
 #' Solve duplicated names by summarizing traits
 #'
 #'
-#' Solve duplicated names by summarizing traits given user provided functions.
+#' Solve duplicated species names by summarizing traits given user provided
+#' functions for common classes of variables (numeric, character, and logical).
 #' 
 #' @param x data.frame.
 #' 
-#' @param duplicated_col The column position with duplicated names to be solved.
+#' @param duplicated_col The number of the column position with duplicated names
+#'  to be solved.
 #' 
 #' @param fixed_cols The columns positions that should be left out of the 
-#' summarizing processes.
+#' summarizing processes. Normally applies for columns with fixed values across 
+#' repeated names. 
 #' 
 #' @param func_numeric A function to summarize numeric columns 
 #' if solve_duplicated = TRUE. Default will return the mean.
@@ -20,27 +23,43 @@
 #' @param func_logical A function to summarize logical columns 
 #' if solve_duplicated = TRUE.Default will return TRUE if any is TRUE.
 #' 
+#' @details 
+#' The function will combine lines in \code{x} with duplicated names found in 
+#' \code{duplicated_col}. User-defined functions to combine the information in 
+#' \code{x} should take a vector (of length > 2) of the corresponding class
+#'  (numeric, character, and logical) and output only one value of the 
+#'  corresponding class. Factors are transformed into characters. 
+#'  
+#' @return 
+#' A data.frame with the same number of columns in  \code{x} 
+#' and combined duplicated lines according to functions provided.
+#'  
+#' @keywords R-package nomenclature taxonomy vascular plants 
 #' 
-#' @examples \dontrun{
+#' @author 
+#' Bruno Vilela & Alexander Ziska
 #' 
+#' @examples
+#' 
+#' # Create a data.frame with duplicated names and different traits
 #' splist <- sample(LCVP::tab_lcvp$Input.Taxon[1:100])
 #' search <- lcvp_search(splist)
+#' 
 #' x <- data.frame("Species" = search$Output.Taxon,
-#' "Trait2" = runif(length(splist)),
-#' "Trait3" = runif(length(splist)),
-#' "Trait4" = sample(c("a", "b"), length(splist), replace = TRUE),
-#' "Trait5" = sample(c(TRUE, FALSE), length(splist), replace = TRUE))
+#' "Trait1" = runif(length(splist)),
+#' "Trait2" = sample(c("a", "b"), length(splist), replace = TRUE),
+#' "Trait3" = sample(c(TRUE, FALSE), length(splist), replace = TRUE))
 #'
 #' # Solve with default parameters
 #' lcvp_solve_dups(x, 1)
 #'
-#' # Summarize numbers using the mean
+#' # Summarize numbers using the median
 #' lcvp_solve_dups(x, 1, func_numeric = median)
 #' 
 #' # Get one of characters at random
 #' lcvp_solve_dups(x, 1, func_character = function(x){sample(x, 1)})
 #' 
-#' }
+#' 
 #'@export
 
 
