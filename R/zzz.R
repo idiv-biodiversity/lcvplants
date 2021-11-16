de3978be014ca0c6b2e22ec0d1007894d64198f9
Bruno Vilela <brunovilelasilva@hotmail.com>
2021-11-16 19:56:21 -0300

    Update dependence on specific version of LCVP

--- R/zzz.R
+++ R/zzz.R
@@ -3,14 +3,17 @@
 
 .onLoad  <- function(libname, pkgname) {
   has_data <- requireNamespace("LCVP", quietly = TRUE)
+  if (has_data) {
+    has_data <- has_data & packageVersion("LCVP") >= 2 # change accordingly
+  }
   .pkgenv[["has_data"]] <- has_data
 }
 
 .onAttach <- function(libname, pkgname) {
   if (!.pkgenv$has_data) {
     msg <- paste("To use this package, you must install the",
-                 "LCVP package. To install that package, run",
-                 "`devtools::install_github('idiv-biodiversity/LCVP')`.",
+                 "LCVP package version 2.0 or higher. To install LCVP package,", 
+                 "run `devtools::install_github('idiv-biodiversity/LCVP')`.",
                  "See the `LCVP` vignette for more details.")
     msg <- paste(strwrap(msg), collapse="\n")
     packageStartupMessage(msg)
@@ -19,13 +22,17 @@
 
 hasData <- function() {
   has_data <- requireNamespace("LCVP", quietly = TRUE)
+  if (has_data) {
+    has_data <- has_data & packageVersion("LCVP") >= 2 # change accordingly
+  }
   if (!has_data) {
     msg <- paste("To use this function, you must have the",
-                 "`LCVP` package installed.",
-                 "To install that package, run",
+                 "`LCVP` package version 2.0 or higher installed.",
+                 "To install LCVP package, run",
                  "`devtools::install_github('idiv-biodiversity/LCVP')`.",
                  "See the `LCVP` package vignette for more details.")
     msg <- paste(strwrap(msg), collapse="\n")
     stop(msg)
   }
 }
+
